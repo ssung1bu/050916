cc75dadc3612b161df67ee4b5c01af79b564c63c
Sarah Sung <mssarahsung@gmail.com>
2016-05-14 14:29:37 -0400

    add clear button and empty string input field

--- index.js
+++ index.js
@@ -1,18 +1,22 @@
 $(document).ready(
 	function(){
-		//$('h1').css('background-color','green');
 	var button = $("#button-1");
 	button.click(function(){
 		var value = $("#field-1").val();
-		display(value);
-		});
+		//display(value);
+		display("");
+	});
 	$('#button-2').click(function(){
 		var value = $("#field-2").val();
-		display(value);
+		//display(value);
+		display("");
 	});
-
+	$('#button-3').click(function(){
+		$("#display").empty()
+	});
 	function display(argument){
 		$("#display").append("<li class='thingy'>" + argument + "</li>");
 	}
+
 });
 	
